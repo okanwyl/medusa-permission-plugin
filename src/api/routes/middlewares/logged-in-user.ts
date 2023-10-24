6bb7a62566703c55a710f2c113efd930094644a0
Okan Akyol <okanakyol@proton.me>
2023-10-25 02:24:52 +0300

    cool stuff

--- src/api/routes/middlewares/logged-in-user.ts
+++ src/api/routes/middlewares/logged-in-user.ts
@@ -1,31 +1,34 @@
-import { UserService } from "@medusajs/medusa"
+import {UserService} from "@medusajs/medusa"
 import {User} from "../../../models/user";
 import PermissionsService from "../../../services/permissions";
 
 export async function permissionMiddleware(req, res, next) {
-  let loggedInUser: User | null = null
+    let loggedInUser: User | null = null
 
-  if (req.user && req.user.userId) {
-    const userService = req.scope.resolve("userService") as UserService
-    // @ts-ignore
-    loggedInUser = await userService.retrieve(req.user.userId,  {relations: ["policy_cluster"]})
-  }
-
-
-  const permissionsService : PermissionsService = req.scope.resolve("permissionsService") as PermissionsService
-
-  // const policies = await permissionsService.fetchPolicies()
-  console.log(permissionsService.policiesHashmap)
-  console.log(loggedInUser)
-  req.scope.register({
-    loggedInUser: {
-      resolve: () => loggedInUser,
-    },
-  })
+    if (req.user && req.user.userId) {
+        const userService = req.scope.resolve("userService") as UserService
+        // @ts-ignore
+        loggedInUser = await userService.retrieve(req.user.userId, {relations: ["policy_cluster"]})
+    }
 
 
-  console.log(req.originalUrl)
-  console.log(req.method)
+    const permissionsService: PermissionsService = req.scope.resolve("permissionsService") as PermissionsService
 
-  next()
+    // const policies = await permissionsService.fetchPolicies()
+    // console.log(await permissionsService.createPolicyHashmap());
+    await permissionsService.init();
+    console.log(permissionsService.policiesHashmap);
+
+    console.log(loggedInUser)
+    req.scope.register({
+        loggedInUser: {
+            resolve: () => loggedInUser,
+        },
+    })
+
+
+    console.log(req.originalUrl)
+    console.log(req.method)
+
+    next()
 }
--- src/services/permissions.ts
+++ src/services/permissions.ts
@@ -5,13 +5,15 @@ import {
 import {IEventBusService} from "@medusajs/types"
 import {Policy} from "../models/policy"
 import PolicyRepository from "../repositories/policy";
+import PolicyClusterRepository from "../repositories/policy-cluster";
 
 export default class PermissionsService extends TransactionBaseService {
 
     static LIFETIME = Lifetime.SINGLETON
-    protected readonly policyRepository_: typeof PolicyRepository
+    protected readonly policyClusterRepository_: typeof PolicyClusterRepository
     protected readonly eventBusService_: IEventBusService
-    private readonly _policiesHashmap: Map<string, Policy[]>;
+    private _policiesHashmap: Map<string, Policy[]>;
+    private _initialized: boolean = false;
 
     static readonly Events = {
         CREATED: "permissions.service.updated",
@@ -21,23 +23,40 @@ export default class PermissionsService extends TransactionBaseService {
         // @ts-ignore
         super(...arguments)
         this.eventBusService_ = container.eventBusService
-        this.policyRepository_ = container.policyRepository
-        this._policiesHashmap = new Map();
-    }
-
-
-    async fetchPolicies() {
-        const policiesRepository = this.activeManager_.withRepository(
-            this.policyRepository_
-        )
-
-        const policies = await policiesRepository.find({});
-
-        return policies;
+        this.policyClusterRepository_ = container.policyClusterRepository
+        this._policiesHashmap = new Map<string, Policy[]>();
 
     }
+
+    async init(): Promise<void> {
+        if (this._initialized) return;  // If already initialized, return immediately
+
+        try {
+            await this.createPolicyHashmap();
+            this._initialized = true;  // Set the flag after successful initialization
+        } catch (err) {
+            console.error(`Cluster Hashmap creation error: ${err}`);
+        }
+    }
+
     get policiesHashmap(): Map<string, Policy[]> {
         return this._policiesHashmap;
+    }
+
+    async createPolicyHashmap(): Promise<void> {
+        const clusterIdMap = new Map<string, Policy[]>();
+
+        const policyClusterRepository = this.activeManager_.withRepository(
+            this.policyClusterRepository_
+        )
+
+        const clusters = await policyClusterRepository.find({relations: ["policy"]});
+
+        clusters.forEach(cluster => {
+            clusterIdMap.set(cluster.id, cluster.policy);
+        });
+
+        this._policiesHashmap = clusterIdMap;
     }
 
 }
